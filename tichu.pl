c2c4ee47cbd12184ac1d08efc7909e9d661421b9
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2021-03-04 01:09:49 +0100

    Clean up code, use English.

--- tichu.pl
+++ tichu.pl
@@ -1,107 +1,125 @@
-% Domena
+% List the players of the tournament.
+players([adam, cezary, eliza, hubert, irena, mateusz, natalia, sonia, tadeusz, urszula]).
 
-names([adam, cezary, eliza, hubert, irena, mateusz, natalia, sonia, tadeusz, urszula]).
+% A player is one of the players.
+player(X) :- players(P), member(X, P).
 
-player(X) :- names(N), member(X, N).
-
+% List all men.
 man(adam).
 man(cezary).
 man(hubert).
 man(mateusz).
 man(tadeusz).
 
-woman(X) :- player(X), \+ man(X).
+% A woman is a player who is not a man.
+woman(X) :-
+    player(X),
+    \+ man(X).
 
-specjalne([smok, mah-jong, psy, feniks]).
-sekwensy([od2, od3, od4, od5]).
-kolory([czarny, zielony, niebieski, czerwony]).
+% Special Tichu cards that are mentioned in the puzzle.
+special_cards([dragon, mah-jong, dog, phoenix]).
 
-% Funkcje pomocnicze
+% Straight that were obtained in the final.
+straights([from2, from3, from4, from5]).
 
+% Possible colours of the straights.
+colours([black, green, blue, red]).
+
+% A helper function – applies an arity-2 predicate, swapping its arguments
 swap(P, X, Y) :- call(P, Y, X).
 
-% Zadanie
+% Find out the pairs and their standings in the tournament.
+pairs_in_order(Pairs) :-
+    % Bring all players into scope.
+    players(Players),
 
-miejsca(Pairs) :-
-    names(Names),
-    % jest 5 zestawów danych
+    % There are 5 pairs in the tournament.
     length(Pairs, 5),
-    % kazdy ma dwie dane – imiona ludzi w parze
     maplist(swap(length, 2), Pairs),
-    
-    % Mateusz jest w parze z Ireną
+
+    % Mateusz is paired with Irena.
     member([mateusz, irena], Pairs),
-    % Nie zajęli ani 3 ani 5 miejsca
+
+    % They were neither 3rd nor 5th, which means they were 1st, 2nd or 4th.
     member(MatIrIndex, [1, 2, 4]),
     nth1(MatIrIndex, Pairs, [mateusz, irena]),
-    
-    % zestawy muszą się sumować do wszystich graczy
+
+    % All players must be present in the tournament.
     flatten(Pairs, Flattened),
-    permutation(Names, Flattened),
-    
-    % Tadeusz jest w parze z kobietą
+    permutation(Players, Flattened),
+
+    % Tadeusz is paired with a woman.
     member([tadeusz, A], Pairs),
     woman(A),
-    % Adam jest w parze z mężczyzną
+
+    % Adam is paired with another man.
     member([adam, B], Pairs),
     man(B),
 
-    % Natalia była wyżej od Cezarego
+    % Natalia finished higher than Cezary.
     nth1(IxNat, Pairs, [_, natalia]),
     nth1(IxCez, Pairs, [_, cezary]),
     IxNat < IxCez,
-    % Sonia była wyżej od Tadeusza
+
+    % Sonia finished higher than Tadeusz.
     nth1(IxSon, Pairs, [_, sonia]),
     nth1(IxTad, Pairs, [tadeusz, _]),
     IxSon < IxTad,
 
-    % W finale (dwie najlepsze pary) był jeden facet
-    % "W zwycięskiej parze mieszanej to Eliza...""
-    % Urszula jest wicemistrzynią
+    % In the final game (between the two best pairs), there was only one man.
+    % Elisa was part of the the winning, mixed pair.
     [[SomeMan, eliza], [F3, F4] | _] = Pairs,
     man(SomeMan),
-    (F3 = urszula; F4 = urszula)
-    .
 
-% ?- miejsca(X).
+    % Urszula is one of the runner-ups.
+    (F3 = urszula; F4 = urszula).
+
+% Executing this query gives us only one possible arrangement:
+
+% ?- pairs_in_order(X).
 % X = [[hubert, eliza], [urszula, sonia], [tadeusz, natalia], [mateusz, irena], [adam, cezary]] ;
 % false.
 
-karcioszki(Data) :-
-    sekwensy(Sekwensy),
-    kolory(Kolory),
-    specjalne(Specjalne),
+% Now we want to know which finalists had which cards.
+players_cards(Data) :-
+    % Bring card info into scope.
+    straights(Straights),
+    colours(Colours),
+    special_cards(SpecialCards),
 
-    % są cztery zestawy danych
+    % There are four finalists. Each has a special card and a straight from a given card and with a given colour.
+    % We know the order of the finalists from running the previous query.
     [[hubert, HSpec, HC, HSek], [eliza, ESpec, EC, ESek], [urszula, USpec, UC, USek], [sonia, SSpec, SC, SSek]] = Data,
-    
-    % karty specjalne, sekwensy i ich kolory są unikatowe
-    permutation(Specjalne, [HSpec, ESpec, USpec, SSpec]),
-    permutation(Kolory, [HC, EC, UC, SC]),
-    permutation(Sekwensy, [HSek, ESek, USek, SSek]),
 
-    % Sekwensy posiadaczki feniksa i jedynego mężczyzny w finale nie zaczynały się od 4
-    \+ member([_, feniks, _, od4], Data),
-    HSek \= od4,
+    % No two players can have the same special card, straight sequence or colour.
+    permutation(SpecialCards, [HSpec, ESpec, USpec, SSpec]),
+    permutation(Straights, [HSek, ESek, USek, SSek]),
+    permutation(Colours, [HC, EC, UC, SC]),
 
-    % W zwycięskiej parze (..) to Eliza miała mah-jonga a ich sekwensy nie były niebieskie
+    % The straights of the only man in the final game (Hubert) and the owner of the Phoenix didn't start with 4.
+    \+ member([_, phoenix, _, from4], Data),
+    HSek \= from4,
+
+    % In the winning pair it was Eliza who had the Mah Jong, and their straights weren't blue.
     ESpec = mah-jong,
-    EC \= niebieski,
-    HC \= niebieski,
+    EC \= blue,
+    HC \= blue,
 
-    % Najniższy sekwens nie był ani czerwony ani zielony
-    \+ member([_, _, zielony, od2], Data),
-    \+ member([_, _, czerwony, od2], Data),
+    % The lowest straight was neither red nor green.
+    \+ member([_, _, red, from2], Data),
+    \+ member([_, _, green, from2], Data),
 
-    % Najwyszy sekwens był czarny
-    member([_, _, czarny, od5], Data),
+    % The highest straight was black.
+    member([_, _, black, from5], Data),
 
-    % Psy miała osoba z czerwonym sekwensem
-    member([_, psy, czerwony, _], Data),
+    % The Dog belongs to the person with the red straight.
+    member([_, dog, red, _], Data),
 
-    % Sekwens wicemistrzyni Urszuli był zielony
-    UC = zielony.
+    % Urszula's straight was green.
+    UC = green.
 
-% ?- karcioszki(X).
-% X = [[hubert, psy, czerwony, od3], [eliza, mah-jong, czarny, od5], [urszula, smok, zielony, od4], [sonia, feniks, niebieski, od2]] ;
+% Executing this query gives us, yet again, only one answer.
+
+% ?- players_cards(X).
+% X = [[hubert, dog, red, from3], [eliza, mah-jong, black, from5], [urszula, dragon, green, from4], [sonia, phoenix, blue, from2]] ;
 % false.
